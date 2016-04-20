7c348294722d0792b5a17bb6fb113d52c8f20fc8
epidemicflu <hcedillo2018@bklawtech.com>
2016-04-20 21:22:19 +0000

    hey

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-window.onload = function () { console.log('loaded!') };
+window.onload = function() { 
+	var formElement = document.getElementById('form');
+	var inputElement = document.getElementById('input');
+
+	formElement.onsubmit = function(e){
+		e.preventDefault();
+		var cityName = inputElement.value;
+		getWeather(cityName);
+	};
+	};
+
+function printWeather(event) {
+	var response = JSON.parse(event.target.responseText);
+	document.write(response.main.temp);
+}
+
+function getWeather(cityName) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather' +
+  '?appid=c2f2d170f6f6fc336058e9851edb828c' +
+  '&q=' + cityName +
+  '&units=imperial';
+
+  var xhrObject = new XMLHttpRequest();
+  xhrObject.open('GET', url, true);
+  xhrObject.onload = printWeather;
+  xhrObject.send();
+}
