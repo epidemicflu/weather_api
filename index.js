3d19369e00f7792ac6eeb947bb6461b7d4ad9788
epidemicflu <hcedillo2018@bklawtech.com>
2016-05-04 21:17:31 +0000

    almost done

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
-window.onload = function() { 
-	var formElement = document.getElementById('form');
-	var inputElement = document.getElementById('input');
+// window.onload = function() { 
+// 	var formElement = document.getElementById('form');
+// 	var inputElement = document.getElementById('input');
 
-	formElement.onsubmit = function(e){
-		e.preventDefault();
-		var cityName = inputElement.value;
-		getWeather(cityName);
-	};
-	};
+// 	formElement.onsubmit = function(e){
+// 		e.preventDefault();
+// 		var cityName = inputElement.value;
+// 		getWeather(cityName);
+// 	};
+// 	};
+$(function() {
+	
 
-function printWeather(event) {
-	var response = JSON.parse(event.target.responseText);
-	document.write(response.main.temp);
+	$('form').on('submit', function(event) {
+		event.preventDefault();
+		
+		var userInput = $('input').val();
+		var cities = userInput.split(',');
+
+    for(var i = 0; i < cities.length; i++){
+    	var cityName = cities[i];
+    	getWeather(cityName);
+    	
+    }
+	});
+});
+
+
+function printWeather(response) {
+	
+	var element = $('<p>', {text: response.main.temp })
+	$('body').append(element);
+	
 }
 
 function getWeather(cityName) {
-  var url = 'http://api.openweathermap.org/data/2.5/weather' +
+  var url = 'https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather' +
   '?appid=c2f2d170f6f6fc336058e9851edb828c' +
   '&q=' + cityName +
   '&units=imperial';
+  
+	$.get(url, printWeather);
 
-  var xhrObject = new XMLHttpRequest();
-  xhrObject.open('GET', url, true);
-  xhrObject.onload = printWeather;
-  xhrObject.send();
 }
